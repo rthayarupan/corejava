4f0268534b6a0694008364efb16668fc611e2eb4
Thayarupan <mrthaya@gmail.com>
2019-04-09 15:03:02 +1200

    Clean up

--- prop-loader/src/main/java/com/corejava/Main.java
+++ prop-loader/src/main/java/com/corejava/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(PropertyUtil.getProperty("provider.class"));
+		System.out.println(PropertyUtil.getProperty("renderer.class"));
 		
 	}
 
--- prop-loader/src/main/java/com/corejava/utility/PropertyLoaderUtil.java
+++ prop-loader/src/main/java/com/corejava/utility/PropertyLoaderUtil.java
@@ -6,22 +6,34 @@ import java.util.Properties;
 
 public class PropertyLoaderUtil {
 
+	/**
+	 * package level: com/corejava/utility/data.properties
+	 * resource: data.properties
+	 */
 	public static Properties loadResourceProperties(String fileName) throws Exception{
 		Objects.requireNonNull(fileName);
 		Properties prop = new Properties();		
 		try(InputStream in = PropertyLoaderUtil.class.getClassLoader().getResourceAsStream(fileName)){
-			prop.load(in);
+			if(in!=null) {
+				prop.load(in);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
 		return prop;
 	}
 	
+	/**
+	 * package level: /com/corejava/utility/data.properties
+	 * resource: /data.properties
+	 */
 	public static Properties loadPackageProperties(String fileName) {
 		Objects.requireNonNull(fileName);
 		Properties prop = new Properties();		
 		try(InputStream in = PropertyLoaderUtil.class.getResourceAsStream(fileName)){
-			prop.load(in);
+			if(in!=null) {
+				prop.load(in);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}		
